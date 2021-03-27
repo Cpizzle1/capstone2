56194386d4444dc73ebfecbdfd7ea9916646798d
C.P. Santos <cpsanto@yahoo.com>
2021-03-27 17:34:17 -0400

    restarted on new project

--- src/weather_energy_combinev2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
-
-import scipy.stats as stats
-from pandas.plotting import scatter_matrix
-import statsmodels.api as sm
-import datetime as dt
